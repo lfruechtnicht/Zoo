17fdf10ce1defcb4625e4b74d3a29562b1777b3d
Morten <morten.dahl@gmx.net>
2020-07-15 19:05:37 +0200

    Improved UX

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ from animal import Animal
 from animal_ascii import lion
 
 print("WELCOME TO THE PYTHONIC ZOO!")
+input()
 print("You have no money right now. You could either try to gamble to get money or you try to sneak in.")
+input()
 
-response = input("What do you want to do?")
+response = input("What do you want to do? ")
 
-while response.lower() not in ["gamble", "get money", "sneak in", "sneak"]:
+while response.lower() not in ["gamble", "get money", "sneak in", "sneak", "play"]:
     response = input("I don't understand. Please repeat: ")
 
-if response in ["gamble", "get money"]:
+if response.lower() in ["gamble", "get money", "play"]:
     money = money_gamble()
 
-elif response in ["sneak in", "sneak"]:
-    print("Very brave!")
+elif response.lower() in ["sneak in", "sneak"]:
     sneak_in()
 
